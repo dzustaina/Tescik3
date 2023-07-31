afc3c66fa52173218829837945dafb8072cfd901
Justyna Kozuch <justynakozuch01@gmail.com>
2023-07-31 14:17:33 +0200

    14:17 form validation

--- ClientApp/src/app/user/registration/registration.component.ts
+++ ClientApp/src/app/user/registration/registration.component.ts
@@ -1,3 +1,27 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-registration-form',
+//   templateUrl: './registration.component.html',
+//   styleUrls: ['./registration.component.css']
+// })
+// export class RegistrationComponent {
+//   name: string | undefined;
+//   email: string | undefined;
+//   password: string | undefined;
+
+//   onSubmit() {
+//     console.log('Registration form submitted!');
+//     console.log(`Name: ${this.name}`);
+//     console.log(`Email: ${this.email}`);
+//     console.log(`Password: ${this.password}`);
+//   }
+// }
+
+
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/user.service';
 
